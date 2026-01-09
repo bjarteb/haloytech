df951f0e30941fc391769c6317fb65556b668821
Bjarte Brandt <bjarte.brandt@gmail.com>
2026-01-09 17:01:22 +0100

    Update site URL with actual GitHub username

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	// Set your GitHub Pages URL
 	// Format: https://yourusername.github.io
-	site: 'https://yourusername.github.io',
+	site: 'https://bjarteb.github.io',
 
 	// Set the base path for your repository
 	// If deploying to https://yourusername.github.io/violet-visual/
